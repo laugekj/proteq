910d45943d4cb61baf04daff186d5419a713532d
Jarom98 <benjamin@luthje.eu>
2021-03-16 19:23:31 +0100

    DB

--- test/Program.cs
+++ test/Program.cs
@@ -6,6 +6,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Data;
+using Npgsql; 
 
 namespace test
 {
@@ -13,7 +15,11 @@ namespace test
     {
         public static void Main(string[] args)
         {
+            InsertRecord();
+            Console.Read(); 
             CreateHostBuilder(args).Build().Run();
+       
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +28,39 @@ namespace test
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+       private static void TestConnection()
+       { 
+           using(NpgsqlConnection con=GetConnection())
+           {
+               con.Open();
+               if(con.State==ConnectionState.Open)
+               {
+                   Console.WriteLine("Connected");
+               }
+               
+           }
+       }
+       
+        private static NpgsqlConnection GetConnection()
+        {
+            return new NpgsqlConnection(@"Server=ec2-34-240-75-196.eu-west-1.compute.amazonaws.com;Port=5432;User Id=ongggporwmcbyy;Password=8ce076823bd198d23b9cea1cee785dedfdf384ff0aa0630e93ad6feac2e2fefe;Database=dchglbie3su9s0;sslmode=Require;Trust Server Certificate=true;");
+        }
+
+        private static void InsertRecord()
+        {
+            using (NpgsqlConnection con = GetConnection())
+            {
+                string query = @"insert into public.Students(Name,Fees)values('test4',200.0)";
+                NpgsqlCommand cmd = new NpgsqlCommand(query, con);
+                con.Open();
+                int n = cmd.ExecuteNonQuery();
+                if(n==1)    
+                {
+                    Console.WriteLine("ja tak");
+                }
+
+            }
+        }
     }
 }
