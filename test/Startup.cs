326c46d1c9201213484dc79b0e1ce8b55589c306
Lauge <lakj@itu.dk>
2021-03-16 20:32:52 +0100

    connectionstring..

--- test/Startup.cs
+++ test/Startup.cs
@@ -32,28 +32,11 @@ namespace test
                 configuration.RootPath = "ClientApp/build";
             });
 
+            services.AddDbContext<UserContext>(options =>
+            options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection")));
 
-            //pull in connection string
-           string connectionString = null;
-           string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
-           if (string.IsNullOrEmpty(envVar)){
-               connectionString = Configuration["Connectionstrings:database"];
-           }
-           else{
-               //parse database URL. Format is postgres://<username>:<password>@<host>/<dbname>
-               var uri = new Uri(envVar);
-               var username = uri.UserInfo.Split(':')[0];
-               var password = uri.UserInfo.Split(':')[1];
-               connectionString = 
-               "; Database=" + uri.AbsolutePath.Substring(1) +
-               "; Username=" + username +
-               "; Password=" + password + 
-               "; Port=" + uri.Port +
-               "; SSL Mode=Require; Trust Server Certificate=true;";
-           }
 
-           services.AddDbContext<UserContext>(opt =>
-            opt.UseNpgsql("connectionstring"));
+       
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
