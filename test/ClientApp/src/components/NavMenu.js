42b997103212340528147fbcc6b57549b6426235
Alberte Kjær Wærens <albw@itu.dk>
2021-04-09 16:50:42 +0200

    arbejdet på forsiden, ikke færdig

--- test/ClientApp/src/components/NavMenu.js
+++ test/ClientApp/src/components/NavMenu.js
@@ -3,6 +3,7 @@ import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLi
 import { Link } from 'react-router-dom';
 import './NavMenu.css';
 
+
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
 
