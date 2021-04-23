da89b954e886a056a36b96c1291852f2939782b1
Harry Singh <hars>
2021-04-23 15:16:06 +0200

    added backend: setHasPaid to UserController

--- test/Controllers/UserController.cs
+++ test/Controllers/UserController.cs
@@ -91,6 +91,20 @@ namespace test.Controllers
         return item; 
         }
 
+        [HttpPut("{id}")]
+        [Route("[action]")]
+        public ActionResult<User> setHasPaid(int id, bool hasPaid) 
+        {
+           var entity = _context.Users.FirstOrDefault(e => e.Id == id);
+           if (entity == null) {
+               return BadRequest("id must match id");
+           } else {
+               entity.HasPaid = hasPaid;
+               _context.SaveChanges();
+               return Ok();
+           }
+        }
+
         public bool DoesEmailExists(string mail)
         {
             var tmp = _context.Users.Where(u => u.Email == mail);
