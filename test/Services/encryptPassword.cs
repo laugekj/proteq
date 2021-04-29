ca864a737745f64768f7097c3e964068bb77a5c4
Lauge <lakj@itu.dk>
2021-04-29 17:15:54 +0200

    ups

--- test/Services/encryptPassword.cs
+++ test/Services/encryptPassword.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Web;
-using System.Security.Cryptography;
+
 namespace test.Services
 {
     public class encryptPassword
