f96fa245844ea6defd13abf0c6b47eef51cf05ca
Lauge <lakj@itu.dk>
2021-05-12 15:21:44 +0200

    lidt fetch i frontend

--- test/ClientApp/src/components/Dashboard/Dashboard.js
+++ test/ClientApp/src/components/Dashboard/Dashboard.js
@@ -13,16 +13,28 @@ export default function Dashboard() {
     { id: 2, designId: 2, completed: false },
     { id: 3, designId: 1, completed: false },
   ];
-
+  
+ 
     const [ user, setUser] = useState()
     const [steps, setSteps] = useState(initialSteps)
 
   
     //TODO: GET STEPS FROM SERVER
     function getSteps() {
-        const response = fetch('api/user');
-        const data = response.json();
-        setSteps(data)
+        fetch('api/steps')
+            .then(response => response.json())
+            .then(data => setSteps(data));
+            console.log("tried to fetch");
+        }
+
+
+   function getUser() {
+        fetch('api/steps/').then(response => {
+            return response.json();
+        })
+        .then((responseJson) => {
+             setSteps(responseJson);
+        });
     }
 
     useEffect(()=>{
@@ -31,6 +43,7 @@ export default function Dashboard() {
         if (loggedInUser) {
           const foundUser = JSON.parse(loggedInUser);
           setUser(foundUser);
+          getSteps()
         }
     }, []);
      
@@ -41,6 +54,7 @@ export default function Dashboard() {
         window.location.href = '/CheckoutRedirect'
         } else {
         return (
+           
             
         <div>
             <div class="body">
@@ -49,7 +63,10 @@ export default function Dashboard() {
                     <Button onClick = {() => window.location.href = '/' }>Hjem</Button>
                     <Button onClick = {() => window.location.href = '/Profile' }>Profilside</Button>
                 </center>
-                <div className="stepsContainer">
+            
+                <h2 className="stepsInfo">Du er nået x langt med dine steps</h2>
+            
+                <div className="stepsContainer"> 
                     <StepStep serversteps={steps}></StepStep>    
                 </div>            
             </div>
