8219bb70108fca2438e37b657b00fd610ed63a1c
Simone Haukjær Jakobsen <hasj@itu.dk>
2021-05-21 12:43:12 +0200

    Implemented get file

--- test/Controllers/FileController.cs
+++ test/Controllers/FileController.cs
@@ -15,61 +15,82 @@ namespace test.Controllers
     {
 
 
-    private readonly UserContext _context;
+        private readonly UserContext _context;
 
-    public FileController(UserContext context)
-    {
-        _context = context;    
-    }
-
-    ///api/file
-    [HttpGet] 
-    public ActionResult<List<Step>> GetAll() 
-    {     
-       return _context.Steps.ToList(); 
-    } 
-
-
-    // /api/file/id
-    [HttpGet("{id}", Name = "GetStep")] 
-    public IActionResult GetById(int id) 
-    {    
-        // returns file
-        var step = _context.Steps.Find(id);     
-        if (step == null)    
-        {         
-            return NotFound();     
+        public FileController(UserContext context)
+        {
+            _context = context;
         }
 
-        
-        // save locally (not relevant)
-        //System.IO.File.WriteAllBytes(step.FilePath, step.File);
-
-        return Ok(); 
-    }
-
-
-
-    [HttpPost]
-    public ActionResult Post([FromForm] StepModel data)
-    {
-        Console.WriteLine("--- Data --- ");
-        Console.WriteLine("DesignId: " + data.DesignId);
-        Console.WriteLine("Title: " + data.Title);
-        Console.WriteLine("Body: " + data.Body);
-        Console.WriteLine("Video: " + data.Video);
-        Console.WriteLine("FileName: " + data.FileName);
-        Console.WriteLine("FileType: " + data.type);
-        
-        try
+        ///api/file
+        [HttpGet]
+        public ActionResult<List<Step>> GetAll()
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", data.FileName);
+            return _context.Steps.ToList();
+        }
 
-             using (Stream stream = new FileStream(path, FileMode.Create))
+        [HttpGet]
+        public ActionResult<List<FileModel>> GetAllFiles(int id)
+        {
+            return _context.Files.Where(x => x.StepId == id).ToList();
+        }
+
+
+        // /api/file/id
+        //[HttpGet("{id}", Name = "GetStep")]
+        //public IActionResult GetById(int id)
+        //{
+        //    // returns file
+        //    var step = _context.Steps.Find(id);
+        //    if (step == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+
+        //    // save locally (not relevant)
+        //    //System.IO.File.WriteAllBytes(step.FilePath, step.File);
+
+        //    return Ok();
+        //}
+
+        //api/file/getFileDataById/id
+        [HttpGet("{id}")]
+        //[Route("[action]")]
+        public IActionResult GetFileDataById(int id)
+        {
+            var file = _context.Files.FirstOrDefault(x => x.StepId == id);
+            
+            if (file == null)
             {
-                data.FormFile.CopyTo(stream);
-
+                return NotFound();
             }
+            return File(file.FileData, file.FileType);
+            //return file;
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Post([FromForm] StepModel data)
+        {
+            Console.WriteLine("--- Data --- ");
+            Console.WriteLine("DesignId: " + data.DesignId);
+            Console.WriteLine("Title: " + data.Title);
+            Console.WriteLine("Body: " + data.Body);
+            Console.WriteLine("Video: " + data.Video);
+            Console.WriteLine("FileName: " + data.FileName);
+            Console.WriteLine("FileType: " + data.type);
+
+            try
+            {
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", data.FileName);
+
+                using (Stream stream = new FileStream(path, FileMode.Create))
+                {
+                    data.FormFile.CopyTo(stream);
+
+                }
                 // save online 
 
 
@@ -91,55 +112,56 @@ namespace test.Controllers
                 step.Video = data.Video;
 
                 _context.Files.Add(file); // IMPORTANT THAT Table gets inserted file row - or else return error: Object reference not set to an instance of an object.
-                
+
                 step.Files.Add(file);
 
                 _context.Steps.Add(step);
                 _context.SaveChanges();
-                
-            return StatusCode(StatusCodes.Status201Created);
+
+                return StatusCode(StatusCodes.Status201Created);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("File POST execption: " + e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
         }
-        catch (Exception e)
+
+        /*[HttpPost]
+        [Route("[action]")]
+        public ActionResult uploadFile(StepModel data) 
+        { 
+            Console.WriteLine("[DEVELOPER MODE] Backend uploadFile() called");
+            Console.WriteLine("[# DesignId] " + data.DesignId);
+            Console.WriteLine("[# Title] " + data.Title);
+            Console.WriteLine("[# Body] " + data.Body);
+            Console.WriteLine("[# Video] " + data.Video);
+            //Console.WriteLine("[# File] " + data.file);
+
+            _context.Steps.Add(new Step() {
+                DesignId = data.DesignId,
+                Title = data.Title,
+                Body = data.Body,
+                Video = data.Video
+
+            });
+
+            _context.SaveChanges();
+
+            return Accepted();
+        }*/
+
+
+
+
+
+        private string getFileType(string fileName)
         {
-            Console.WriteLine("File POST execption: " + e.Message);
-            return StatusCode(StatusCodes.Status500InternalServerError);
+            string fileType = fileName; // XXXX.jpg
+            fileType = fileType.Substring(fileName.IndexOf('.') + 1); // jpg
+
+            return fileType;
         }
-    }
-
-    /*[HttpPost]
-    [Route("[action]")]
-    public ActionResult uploadFile(StepModel data) 
-    { 
-        Console.WriteLine("[DEVELOPER MODE] Backend uploadFile() called");
-        Console.WriteLine("[# DesignId] " + data.DesignId);
-        Console.WriteLine("[# Title] " + data.Title);
-        Console.WriteLine("[# Body] " + data.Body);
-        Console.WriteLine("[# Video] " + data.Video);
-        //Console.WriteLine("[# File] " + data.file);
-
-        _context.Steps.Add(new Step() {
-            DesignId = data.DesignId,
-            Title = data.Title,
-            Body = data.Body,
-            Video = data.Video
-            
-        });
-
-        _context.SaveChanges();
-
-        return Accepted();
-    }*/
-
-
-    
-
-
-    private string getFileType(string fileName) {
-        string fileType = fileName; // XXXX.jpg
-        fileType = fileType.Substring(fileName.IndexOf('.')+1); // jpg
-
-        return fileType;
-    }
 
     }
 }
