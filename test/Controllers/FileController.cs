98a91c6a686ba4336637b0d8af688908cf47c8cb
Harry Singh <hars>
2021-05-21 22:40:13 +0200

    multiple file upload works with database

--- test/Controllers/FileController.cs
+++ test/Controllers/FileController.cs
@@ -56,47 +56,48 @@ namespace test.Controllers
         [Route("[action]")]
         public IActionResult CreateStep([FromForm] StepModel data)
         {
+            ///////////////////////////////////////////////////////////
             Console.WriteLine("--- Data --- ");
             Console.WriteLine("DesignId: " + data.DesignId);
             Console.WriteLine("Title: " + data.Title);
             Console.WriteLine("Body: " + data.Body);
             Console.WriteLine("Video: " + data.Video);
-            //Console.WriteLine("FileName: " + data.FileName);
-            //Console.WriteLine("FileType: " + data.type);
+            foreach (var formFile in data.FormFiles)
+            {
+                if (formFile.Length > 0)
+                {
+                    Console.WriteLine(formFile.FileName + ", " + formFile.ContentType);
+                }
+            }
             Console.WriteLine("----------------------");
+            ///////////////////////////////////////////////////////////
+
+            try
+            {
+                Step step = new Step();
+                
+
+                string path;
                 foreach (var formFile in data.FormFiles)
                 {
                     if (formFile.Length > 0)
                     {
-                        Console.WriteLine("####");
-                        Console.WriteLine(formFile.FileName + ", " + formFile.ContentType);
+                        path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", formFile.FileName);
+
+                        using (Stream stream = new FileStream(path, FileMode.Create))
+                        {
+                            formFile.CopyTo(stream);
+                        }
+                        FileModel file = new FileModel();
+                        byte[] fileToBytes = System.IO.File.ReadAllBytes(path);
+                        file.StepId = step.Id;
+                        file.Step = step;
+                        file.FileData = fileToBytes;
+                        file.FileType = formFile.ContentType;
+                        _context.Files.Add(file); // IMPORTANT THAT Table gets inserted file row - or else return error: Object reference not set to an instance of an object.
+                        step.Files.Add(file);
                     }
                 }
-            Console.WriteLine("----------------------");
-
-            try
-            {
-                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", data.FileName);
-
-                using (Stream stream = new FileStream(path, FileMode.Create))
-                {
-                    data.FormFiles[0].CopyTo(stream);
-
-                }
-                // save online 
-
-
-                Step step = new Step();
-                Console.WriteLine("1. init file...");
-                FileModel file = new FileModel();
-
-                byte[] fileToBytes = System.IO.File.ReadAllBytes(path);
-                Console.WriteLine("2. input file...");
-                //file.Id = _context.Files.Any() ? _context.Files.Max(p => p.Id) + 1 : 1;
-                file.StepId = step.Id;
-                file.Step = step;
-                file.FileData = fileToBytes;
-                file.FileType = data.type;
                 
                 int tmp_DesignId;
                 bool success = Int32.TryParse(data.DesignId.ToString(), out tmp_DesignId);
@@ -107,13 +108,9 @@ namespace test.Controllers
                 step.Body = data.Body;
                 step.Video = data.Video;
 
-                _context.Files.Add(file); // IMPORTANT THAT Table gets inserted file row - or else return error: Object reference not set to an instance of an object.
-
-                step.Files.Add(file);
-
                 _context.Steps.Add(step);
                 _context.SaveChanges();
-
+                Console.WriteLine("Step has been successfully created!");
                 return StatusCode(StatusCodes.Status201Created);
             }
             catch (Exception e)
