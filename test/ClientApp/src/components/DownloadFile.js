6ee4522ba483d417ce5ede1834206acb5dbad0a6
Simone Haukjær Jakobsen <hasj@itu.dk>
2021-05-25 12:03:25 +0200

    COMMIT

--- test/ClientApp/src/components/DownloadFile.js
+++ test/ClientApp/src/components/DownloadFile.js
@@ -4,75 +4,75 @@ import DownloadLink from "react-download-link";
 import Button from "@material-ui/core/Button";
 
 export function DownloadFile() {
-    //const data = { FileType : FileType, FileData : FileData };
     const [file, setFile] = useState();
+    const [files, setFiles] = useState([]);
 
     useEffect(() => {
         const getFile = localStorage.getItem("file");
         if (getFile != null) {
             const foundFile = JSON.parse(getFile);
             setFile(foundFile);
-            
+
         }
     }, []);
-    function getFiles(id) {
-        
+
+    const getFiles = () =>
+        files.push("fileName1", "fileName2", "fileName3");
+    console.log("HALLO", files);
+
+    function getFile(id) {
+
         fetch('api/file/' + 45, { method: 'GET' }).then(response => {
             return response.blob();
-            })
+        })
             .then((responseJson) => {
-                    setFile(responseJson);
-                    localStorage.setItem('file', JSON.stringify(responseJson))
-                });
-        
+                setFile(responseJson);
+                localStorage.setItem('file', JSON.stringify(responseJson))
+            });
+
     }
+
     function displayFiles() {
         fetch('api/file/' + 45, { method: 'GET' })
             .then(response => {
                 response.blob().then(blob => {
+                    getFile();
                     let url = window.URL.createObjectURL(blob);
                     let a = document.createElement('a');
                     a.href = url;
+
                     a.download = url;
-                    a.click();                 
+                    a.click();
+
                 });
-                
+
             });
     }
 
-    //function getAllFiles({ serverfiles }) {
+    getFiles();
 
-    //    const [files, setFiles] = useState(serverfiles);
 
-    //    const onComplete = (inputFile) => {
-    //        const newFiles = [];
-    //        files.forEach((file) => {
-    //            if (file.id === inputFile.id) {
-    //                file.completed = !file.completed;
-    //                // Do some database stuff
-    //            }
-    //            newFiles.push(file);
-    //        });
-    //        setFiles(newFiles);
-    //    };
-    //}
-    
-return ( 
-    <Container component="main" maxWidth="xs">
-        <h1>Download filer</h1>
-   
+    const listFiles = files.map((files) =>
+        <li>{files}</li>
+    );
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <React.StrictMode>
+                <ul>{listFiles}</ul>
+            </React.StrictMode>
             <DownloadLink
-            label="Download fil"
-            exportFile={() => Promise.resolve(displayFiles())}
-        />
-         
-        
-        <Button
-            fullWidth
-            variant="contained"
-            color="primary"
-            onClick={getFiles}> Hent filer
-        </Button>
-    </Container>
+                label="Download fil"
+                exportFile={() => Promise.resolve(displayFiles())}
+            />
+
+
+            {/*<Button*/}
+            {/*    fullWidth*/}
+            {/*    variant="contained"*/}
+            {/*    color="primary"*/}
+            {/*    onClick={getFile}> Hent filer*/}
+            {/*</Button>*/}
+        </Container>
     )
 }
