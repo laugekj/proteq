4d8cf2c84fa38924df9c53e01ad4720c924f56b0
Louise Gøtzsche <louisegotzsche@MacBook-Pro-tilhrende-Louise.local>
2021-05-25 18:27:35 +0200

    UI forbedringer

--- test/ClientApp/src/components/Footer.js
+++ test/ClientApp/src/components/Footer.js
@@ -2,6 +2,7 @@ import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import React, { Component } from 'react';
 import './Reset.css';
 import './Footer.css';
+import Grid from '@material-ui/core/Grid';
 
 
 export class Footer extends Component {
@@ -27,6 +28,35 @@ export class Footer extends Component {
                       </li>
                     </ul>
                 </MDBRow>
+                <MDBContainer id = 'footermiddle' fluid className="text-md-left"></MDBContainer>
+                <MDBRow id ='footerRow'>
+                <Grid container spacing={2}>
+                        <Grid item xs={1} sm={1} id="tekst0">
+                        </Grid>
+
+                        <Grid item xs={2} sm={2} id="tekst1">
+                            <p>Vores viden og erfaring inden <br />
+                            for IT og jura bruger vi i arbejdet <br />
+                            med GDPR, således at I får de sikreste <br />
+                            løsninger og overholder GDPR så enkelt <br />
+                            som muligt.</p>
+                        </Grid>
+
+                        <Grid item xs={1} sm={3} id="tekst2">
+                            <p>Du kan altid kontakte os på +45 45 45 45 45</p>
+                        </Grid>
+
+                        <Grid item xs={1} sm={2} id="tekst3">
+                        <p>Hvad er proteQ?</p>
+                        </Grid>
+
+                        <Grid item xs={1} sm={2} id="tekst3">
+                        <p>kontakt os på mail: info@proteq.com
+                        eller på telefon: +45 45 45 45 45</p>
+                        </Grid>
+                        </Grid>
+                </MDBRow>
+                
               </MDBContainer>
               <div  id = 'buttomFooter' className="footer-copyright text-center py-3">
                 <MDBContainer fluid>
