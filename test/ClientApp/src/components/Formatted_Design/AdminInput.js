5aacf171f249103369b29e507ea65cae59771a21
Jarom98 <benjamin@luthje.eu>
2021-05-26 11:00:20 +0200

    haha

--- test/ClientApp/src/components/Formatted_Design/AdminInput.js
+++ test/ClientApp/src/components/Formatted_Design/AdminInput.js
@@ -12,59 +12,41 @@ const AttachDocumentStyle = {
     display: 'flex',
   };
 
-export class AdminInput extends React.Component {
-    constructor(probs) {
-        super(probs);
-        
-        this.state = {
-            id: -1,
-            designId: 0,
-            header: "",
-            body: "",
-            video: "",
-            files: [],
-          };
+export default function AdminInput() {
 
-        this.uploadToServer = this.uploadToServer.bind(this);
-        this.onChange = this.onChange.bind(this);
-        this.getStep = this.getStep.bind(this);
+    const [ id, setId] = useState(-1)
+    const [ designId, setDesignid] = useState(0)
+    const [ header, setHeader] = useState("")
+    const [ body, setBody] = useState("")
+    const [ video, setVideo] = useState("")
+    const [ files, setFiles] = useState([])
 
-      }
-      componentDidMount() {
-        this.getStep();
-        
+    function getStep() {
+        const urlstring = window.location.href;
+        this.setState({id: urlstring.split('?')[1]})
+
+        fetch('api/file/' + this.state.id, { method: 'GET' }).then(response => {
+        return response.json();
+    })
+    .then((responseJson) => {
+        console.log(responseJson.body)
+        this.setState({header: responseJson.title, body: responseJson.body});
+        console.log(this.state.header)
+        console.log(urlstring);
+        console.log(this.state.id);
+    });
     }
 
-      getStep() {
-          const urlstring = window.location.href;
-          this.setState({id: urlstring.split('?')[1]})
-
-          fetch('api/file/' + this.state.id, { method: 'GET' }).then(response => {
-          return response.json();
-      })
-      .then((responseJson) => {
-          console.log(responseJson.body)
-          this.setState({header: responseJson.title, body: responseJson.body});
-          console.log(this.state.header)
-          console.log(urlstring);
-          console.log(this.state.id);
-      });
-
-   }
-
- submit(){
+    function submit(){
         
-    if(this.state.id > -1) {
-        console.log("Editing step")
+        if(this.state.id > -1) {
+            console.log("Editing step")
+        }
+        else{
+            console.log("Creating step")
+        }
     }
-    else{
-        console.log("Creating step")
-    }
-    
-}
-
-
-      uploadToServer = async (e) => {
+    function uploadToServer (){
         console.log("uploadToServer: ", this.state.files);
         const formData = new FormData();
         formData.append("designId", this.state.designId);
@@ -78,42 +60,26 @@ export class AdminInput extends React.Component {
 
         formData.append("formFiles", this.state.files);
         try {
-          const res = await axios.post("http://localhost:5000/api/file/CreateStep", formData);
+          const res =  axios.post("http://localhost:5000/api/file/CreateStep", formData);
           console.log(res);
         } catch (ex) {
           console.log(ex);
         }
       }
-
-      /*uploadToServer = () => {
-          const data = {DesignId: this.state.designId, Title: this.state.header, Body: this.state.body, Video: this.state.video, Files: this.state.files}
-          console.log('[DEVELOPER MODE] UPLOAD TO SERVER FUNCTION CALLED!')
-          fetch('api/file/uploadfile', {
-              method: 'POST',
-              headers: {
-                  'Content-Type': 'application/json',
-              },
-              body: JSON.stringify(data)
-          }).then(response => {
-              console.log('SERVER RESPONSE: ', response)
-          });
-      }*/
-      onChange(e) {
-        var files = e.target.files;
+      function onChange(e) {
+        setFiles(e.target.files); 
         var filesArr = Array.prototype.slice.call(files);
         this.setState({ files: [...this.state.files, ...filesArr] });
     
       }
       
-      removeFile(f) {
+      function removeFile(f) {
            this.setState({ files: this.state.files.filter(x => x !== f) }); 
       }
-
-      render() {
-    return (
+      return (
         <Container>
             <Button onClick={e => this.getStep()}>test</Button> 
-            <h1>{this.props.title}</h1>
+          
         <Grid
             direction="column"
             justify="flex-start"
@@ -122,7 +88,7 @@ export class AdminInput extends React.Component {
                     <FormGroup>
                         <Label for="exampleHeader">Header</Label>
                         <Input 
-                        value={this.state.header} 
+                        value={header} 
                         onChange={e =>  this.setState({ header: e.target.value})} 
                         name="header" 
                         id="exampleHeader" 
@@ -134,25 +100,18 @@ export class AdminInput extends React.Component {
                         type="textarea" 
                         name="text" 
                         id="bodyText"
-                        value={this.state.body} 
+                        value={body} 
                         onChange={e => this.setState({ body: e.target.value})}  />
                     </FormGroup>
                     <Row form>
                         <Col md={6}>
-                            <FormGroup>
-                                <Label for="bulletPoint">Bulletpoint</Label>
-                                <Input 
-                                id="bulletPoint" 
-                                placeholder="Input one bulletpoint at a time"
-                                value={this.state.point}
-                                />
-                            </FormGroup>
+                           
                             <FormGroup>
                                 <Label for="videoLink">Videolink</Label>
                                 <Input 
                                 id="videoLink" 
                                 placeholder="Indsæt videolink"
-                                value={this.state.video}
+                                value={video}
                                 onChange={e => this.setState({ video: e.target.value})} />
                             </FormGroup>
                         </Col>
@@ -175,23 +134,22 @@ export class AdminInput extends React.Component {
                         <FormText color="muted">
                             <div style={AttachDocumentStyle}>
                             <label className="custom-file-upload">
-                            <input type="file" multiple onChange={this.onChange} />
+                            <input type="file" multiple onChange={onChange} />
                             <i className="fa fa-cloud-upload" /> Tilføj dokument
                             </label>
-                            {this.state.files.map(x => 
+                            {files.map(x => 
                             <div className="file-preview" onClick={this.removeFile.bind(this, x)}>{x.name}</div>
                             )}
                         </div>                        
                         </FormText>
                     </FormGroup>
-                    <Button onClick={this.uploadToServer}>Submit</Button>
+                    <Button onClick={uploadToServer}>Submit</Button>
                     </Form>
             
 
             </Grid>
-            <iframe class="video" width="600" height="350" src={this.state.video} title="YouTube video player" frameborder="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            <iframe class="video" width="600" height="350" src={video} title="YouTube video player" frameborder="" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </Container>          
     );
-}
 }
 
