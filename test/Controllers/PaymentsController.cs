9776f83fce67d64fb1917d472fc4c1455e32df1f
Harry Singh <hars>
2021-05-27 20:22:20 +0200

    stripe bug fixed + removed writeline

--- test/Controllers/PaymentsController.cs
+++ test/Controllers/PaymentsController.cs
@@ -63,7 +63,7 @@ namespace test.Controllers
         },
         Mode = "payment",
         SuccessUrl = "http://localhost:5000/success?" + update(user.Id),
-        CancelUrl = "http://localhost:5000/checkout",
+        CancelUrl = "http://localhost:5000",
       };
       
       var service = new SessionService();
@@ -82,9 +82,6 @@ namespace test.Controllers
           // Confirmation (hasPaid=true)
           entity.HasPaid = true;
 
-          // Delete the paymentToken
-          entity.PaymentToken = null;
-
           // Save the changes
           _context.SaveChanges();
       }
@@ -93,90 +90,6 @@ namespace test.Controllers
 
    }
 
-
-      [HttpPut("{id}")]
-      [Route("[action]")]
-      public ActionResult<User> VerifyPaymentToken(PaymentToken pt)
-      {
-          // Retrieve the paymentToken from the URL and save it
-          pt.paymentToken = retrievePaymentTokenFromURL(pt.url);
-
-          // Check if a user (entity) exists witht he given paymentToken:
-          var entity = _context.Users.FirstOrDefault(e => e.PaymentToken == pt.paymentToken);
-          
-          // check if user is found
-          if (entity == null) 
-          {
-            return BadRequest("PaymentToken doesn't exists in the Database");
-          } 
-          
-          // Confirmation (hasPaid=true)
-          entity.HasPaid = true;
-
-          // Delete the paymentToken
-          entity.PaymentToken = null;
-
-          // Save the changes
-          _context.SaveChanges();
-          
-          
-          return entity;
-      }
-
-
-    [HttpPut("{id}")]
-    [Route("[action]")]
-    public ActionResult createPaymentToken(User user) 
-    {
-      // find user: entity
-      var entity = _context.Users.FirstOrDefault(e => e.Id == user.Id);
-      
-      // check if user is found
-      if (entity == null) {
-          return BadRequest("id must match id");
-      } else {
-      // generate payment token
-      this.paymentToken = generatePaymentToken(user.Id);
-
-      // update database PaymentToken
-      entity.PaymentToken = this.paymentToken;
-
-      // save changes
-      _context.SaveChanges();
-      return Ok();
-      }
-    }
-
-
-    private string retrievePaymentTokenFromURL(string URL) 
-    {
-      // http://your-website.com/success?XXXXX-paymentToken-XXXXX
-      // return everything after first occurrence of ' ? '
-      int startIndex = URL.IndexOf('?') + 1;
-      int endIndex = URL.Length - startIndex;
-      return URL.Substring(startIndex, endIndex);;
-    }
-
-    private string generatePaymentToken(int userId) 
-    {   
-      // example of payment token generated date 28 / 04 / 2021 with userId 19
-      // pt_28042021_uid_19_XXXXXX__64_Characters_Long__XXXXXX
-      string paymentToken = "pt_" + DateTime.Now.ToString("ddMMyyyy") + "_";
-      paymentToken += "uid_" + userId + "_";
-      paymentToken += RandomString(64);
-
-
-      return paymentToken;
-    }
-
-    private static Random random = new Random();
-    public static string RandomString(int length)
-    {
-        // Created with help from: https://stackoverflow.com/a/1344242
-        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        return new string(Enumerable.Repeat(chars, length)
-          .Select(s => s[random.Next(s.Length)]).ToArray());
-    }
 
   }
 }
