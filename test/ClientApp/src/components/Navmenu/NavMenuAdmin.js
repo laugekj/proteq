9838b5ed8d7c110f07cc67a595e863b652fdb204
Harry Singh <hars>
2021-05-28 12:43:01 +0200

    fuck albert

--- test/ClientApp/src/components/Navmenu/NavMenuAdmin.js
+++ test/ClientApp/src/components/Navmenu/NavMenuAdmin.js
@@ -41,7 +41,7 @@ export class NavMenuAdmin extends Component {
             <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow">
                 <NavItem>
-                  <NavLink tag={Link} className="nav-text" to="/dashboardAdmin">Startside</NavLink>
+                  <NavLink tag={Link} className="nav-text" to="/dashboard">Startside</NavLink>
                 </NavItem>
                 <NavItem>
                   <NavLink tag={Link} className="nav-text" to="/admininput">Opret et step</NavLink>
